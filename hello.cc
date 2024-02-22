202d4be702c8b485907623bd7ef802d14aa24a68
Vince Herried <vherried@gmail.com>
2024-02-22 12:22:51 -0500

    Version 2.1

--- hello.cc
+++ hello.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 int main()
 {
-	float version=1.1;
+	float version=2.1;
 	std::cout<< "Helow world" << std::endl;
 	std::cout<< "Version: " << version << std:: endl;
 	return 0;
