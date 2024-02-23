07d4b7fcaee8a394c14ad2fc79cd1526a8104f54
Vince Herried <vherried@gmail.com>
2024-02-22 21:36:22 -0500

    tmp branch change 292?

--- hello.cc
+++ hello.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 int main()
 {
-	float version=192.1;
+	float version=292.1;
 	std::cout<< "Helow world" << std::endl;
 	std::cout<< "Version: " << version << std:: endl;
 	return 0;
